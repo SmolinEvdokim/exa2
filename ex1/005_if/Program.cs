88abd1a805720ecdbfb0bb490a77e52a6ef3bcd2
Smolin <smolinevdokim@gmail.com>
2022-12-02 23:31:48 +0100

    delenie 005 if else

---
+++ ex1/005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Schreib Username: ");
+string username = Console.ReadLine();
+
+ if(username.ToLower() == "mascha")
+ {
+    Console.WriteLine("Hura das ist Mascha");
+ }
+ else;
+ {
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+ }
